0a586394a6934c5431443dd13268cd4a1db764f2
rochabianca <bianca.rocha1711@gmail.com>
2020-09-14 18:39:51 -0300

    working!

--- tip.py
+++ tip.py
@@ -2,17 +2,12 @@ import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 
-# Cria as variáveis do problema
 foodRating = ctrl.Antecedent(np.arange(0, 11, 1), 'foodRating')
 serviceRating = ctrl.Antecedent(np.arange(0, 11, 1), 'serviceRating')
 tip = ctrl.Consequent(np.arange(0, 26, 1), 'tip')
 
-# Cria automaticamente o mapeamento entre valores nítidos e difusos
-# usando uma função de pertinência padrão (triângulo)
-foodRating.automf(names=['péssima', 'comível', 'deliciosa'])
+foodRating.automf(names=['horrível', 'ok', 'deliciosa'])
 
-
-# Cria as funções de pertinência usando tipos variados
 serviceRating['ruim'] = fuzz.trimf(serviceRating.universe, [0, 0, 5])
 serviceRating['aceitável'] = fuzz.gaussmf(serviceRating.universe, 5, 2)
 serviceRating['excelente'] = fuzz.gaussmf(serviceRating.universe, 10,3)
@@ -23,15 +18,19 @@ tip['alta'] = fuzz.trimf(tip.universe, [15, 25, 25])
 
 rule1 = ctrl.Rule(serviceRating['excelente'] | foodRating['deliciosa'], tip['alta'])
 rule2 = ctrl.Rule(serviceRating['aceitável'], tip['média'])
-rule3 = ctrl.Rule(serviceRating['ruim'] & foodRating['péssima'], tip['baixa'])
+rule3 = ctrl.Rule(serviceRating['ruim'] & foodRating['horrível'], tip['baixa'])
 
 tip_ctrl = ctrl.ControlSystem([rule1, rule2, rule3])
 tip_simulador = ctrl.ControlSystemSimulation(tip_ctrl)
 
-# Entrando com alguns valores para qualidade da foodRating e do serviço
-tip_simulador.input['foodRating'] = 3.5
-tip_simulador.input['serviceRating'] = 9.4
+print('Qual a nota da comida? (0-10)')
+tip_simulador.input['foodRating'] = int(input())
+print('Qual a nota do serviço? (0-10)')
+tip_simulador.input['serviceRating'] = int(input())
+print('Qual o valor da conta?
+totalPrice = int(input())
 
-# Computando o resultado
 tip_simulador.compute()
-print(tip_simulador.output['tip'])
+tipPercent = round(tip_simulador.output['tip'])
+tipValue = totalPrice*tipPercent/100
+print('Sua gorgeta é: ' + str(tipPercent)+ '%, ou R$'+ str(tipValue))
